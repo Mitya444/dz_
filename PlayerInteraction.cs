1d037ba6886dd562dcf7801542431b5bd8d06d7b
Mitya444 <85624789+Mitya444@users.noreply.github.com>
2025-01-24 14:26:33 +0200

    Додав файл

---
+++ PlayerInteraction.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerinteraction : MonoBehaviour
+{
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            float interactRange = 3.5f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider collider in colliderArray)
+            {
+                if (collider.TryGetComponent(out NPcinteraction npcInteractable))
+                {
+                    npcInteractable.Interact();
+                }
+            }
+        }
+    }
+}
